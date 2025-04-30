f19bcf77e826a0646676c5f095ba88687c02a7e0
Kuldeep Kumar <kuldeep.kumar@gatech.edu>
2025-04-29 23:42:50 -0400

    Updated logic to add hash surrogate keys

--- dbt/lapd_crime_project/models/marts/core/dim_date.sql
+++ dbt/lapd_crime_project/models/marts/core/dim_date.sql
@@ -3,24 +3,25 @@
     schema='mart_core'
 ) }}
 
-with date_ as (
+WITH date_ AS (
     {{ dbt_utils.date_spine(
         datepart="day",
         start_date="cast('2019-01-01' as date)",
         end_date="cast('2030-12-31' as date)"
     ) }}
 )
-select
-    row_number() over (order by date_day) as id,
-    date_day as full_date,
-    extract(year from date_day) as year,
-    extract(month from date_day) as month,
-    upper(to_char(date_day, 'MON')) as month_name,  -- Using 'MON' for 3-letter month abbreviation
-    extract(day from date_day) as day,
-    extract(dow from date_day) as day_of_week,
-    to_char(date_day, 'Day') as day_name,
-    extract(week from date_day) as week_of_year,
-    extract(quarter from date_day) as quarter,
-    case when extract(dow from date_day) in (0, 6) then true else false end as is_weekend,
-    current_timestamp() as doe
-from date_
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['date_day']) }} AS id,
+    date_day AS full_date,
+    EXTRACT(year FROM date_day) AS year,
+    EXTRACT(month FROM date_day) AS month,
+    UPPER(TO_CHAR(date_day, 'MON')) AS month_name,
+    EXTRACT(day FROM date_day) AS day,
+    EXTRACT(dow FROM date_day) AS day_of_week,
+    TO_CHAR(date_day, 'Day') AS day_name,
+    EXTRACT(week FROM date_day) AS week_of_year,
+    EXTRACT(quarter FROM date_day) AS quarter,
+    CASE WHEN EXTRACT(dow FROM date_day) IN (0, 6) THEN TRUE ELSE FALSE END AS is_weekend,
+    CURRENT_TIMESTAMP AS doe
+FROM date_
--- dbt/lapd_crime_project/models/marts/core/dim_time.sql
+++ dbt/lapd_crime_project/models/marts/core/dim_time.sql
@@ -12,29 +12,18 @@ minutes_cte as (
     -- Generate minutes from 0 to 59 for each hour
     select row_number() over (order by seq4()) - 1 as minute
     from table(generator(rowcount => 60))
-),
-time_dim as (
-    select
-        -- Generate a unique ID for each minute in a day
-        row_number() over (order by h.hour, m.minute) as id,
-        h.hour,
-        m.minute,
-        case
-            when h.hour >= 0 and h.hour < 6 then 'Night'
-            when h.hour >= 6 and h.hour < 12 then 'Morning'
-            when h.hour >= 12 and h.hour < 18 then 'Afternoon'
-            when h.hour >= 18 and h.hour < 24 then 'Evening'
-            else 'Unknown'
-        end as part_of_day,
-        current_timestamp() as doe
-    from hours_cte h
-    cross join minutes_cte m
 )
-
-select
-    id,
-    hour as hour_of_day,
-    minute as minute_of_hour,
-    part_of_day,
-    doe
-from time_dim
+    SELECT
+    {{ dbt_utils.generate_surrogate_key(['h.hour', 'm.minute']) }} AS id,
+    h.hour AS hour_of_day,
+    m.minute AS minute_of_hour,
+    CASE
+        WHEN h.hour BETWEEN 0 AND 5 THEN 'Night'
+        WHEN h.hour BETWEEN 6 AND 11 THEN 'Morning'
+        WHEN h.hour BETWEEN 12 AND 17 THEN 'Afternoon'
+        WHEN h.hour BETWEEN 18 AND 23 THEN 'Evening'
+        ELSE 'Unknown'
+    END AS part_of_day,
+    CURRENT_TIMESTAMP AS doe
+FROM hours_cte h
+CROSS JOIN minutes_cte m
